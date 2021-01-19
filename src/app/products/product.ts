0b778958eaba6af1979ba9ef82a6cc6d65aab9c5
Sai Ganesh <ganeshgo1999@gmail.com>
2021-01-19 17:22:46 +0530

    hi

--- src/app/products/product.ts
+++ src/app/products/product.ts
@@ -9,3 +9,18 @@ export interface IProduct{
     imageUrl : string;
 
 }
+
+export class Product implements IProduct{
+    constructor(public productId:number,
+                public productName:string,
+                public productCode:string,
+                public releaseDate:string,
+                public price:number,
+                public description:string,
+                public starRating:number,
+                public imageUrl:string){}
+    calculateDiscount(percent : number):number{
+        return this.price - (this.price*percent/100);
+    }
+
+}
