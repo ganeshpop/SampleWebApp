019d0480b8e73d5b26042815ca253066146c9a0a
Sai Ganesh <ganeshgo1999@gmail.com>
2021-01-20 22:03:33 +0530

    Nested Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
-
+import { StarComponent } from './shared/star.component'
 import { ProductListComponent } from './products/product-list.component';
-import { from } from 'rxjs';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,16 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IProduct} from './product';
 
 @Component({
     selector :'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls:['./product-list.component.css']
 })
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
     pageTitle : string ='Products List';
     imageWidth : number = 50;
     imageMargin : number = 2;
-    listFilter : string = 'cart';
     showImage :boolean = false;
+    _listFilter : string;
+    get listFilter():string{
+        return this._listFilter;
+    }
+    set listFilter(value:string){
+        this._listFilter=value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter):this.products;
+    }
+
+    filteredProducts : IProduct[];
     products : IProduct[] =[
         {
             "productId": 1,
@@ -19,7 +29,7 @@ export class ProductListComponent{
             "releaseDate": "March 19, 2019",
             "description": "Leaf rake with 48-inch wooden handle.",
             "price": 19.95,
-            "starRating": 3.2,
+            "starRating": 3.5,
             "imageUrl": "assets/images/leaf_rake.png"
           },
           {
@@ -29,13 +39,30 @@ export class ProductListComponent{
             "releaseDate": "March 18, 2019",
             "description": "15 gallon capacity rolling garden cart",
             "price": 32.99,
-            "starRating": 4.2,
+            "starRating": 4.1,
             "imageUrl": "assets/images/garden_cart.png"
           }
     ];
+    constructor(){
+        this.filteredProducts = this.products;
+        this._listFilter = 'cart';
+    }
+    onRatingClicked(message: string) : void{
+        this.pageTitle = 'Products List : '+message;
+
+    }
+
+    performFilter(filterBy:string):IProduct[]{
+        filterBy =filterBy.toLocaleLowerCase();
+        return this.products.filter((product:IProduct)=> product.productName.toLocaleLowerCase().indexOf(filterBy)!==-1);
+    }
+
     toggleImage(): void {
         this.showImage = !this.showImage;
-
+   
+    }
+    ngOnInit():void{
+        console.log('In OnInit');
     }
 
 } 
